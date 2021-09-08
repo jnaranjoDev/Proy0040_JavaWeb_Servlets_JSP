680004ab7ad30b434e98a2d95710a48b2c98f2e8
NaranjoDev <49539139+jnaranjoDev@users.noreply.github.com>
2021-09-08 15:03:25 +0200

    Initial commit

---
+++ src/main/java/com/curso/servlets/SaludoServlet.java
@@ -0,0 +1,43 @@
+package com.curso.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SaludoServlet
+ */
+@WebServlet("/SaludoServlet")
+public class SaludoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SaludoServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String nombre = request.getParameter("nombre");
+		System.out.println("EL nombre es "+nombre);
+		response.getWriter().append("LA VARIABLE ES:"+nombre);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
